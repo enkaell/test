83661c9ebfbcddfa9354fe52bc01439327c5ec86
vlad <vlad2012chesnokov@mail.ru>
2022-09-06 21:03:38 +0500

    ss

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from urllib3.util.retry import Retry
 import zipfile
 from fastapi import FastAPI, BackgroundTasks
 from fastapi.responses import FileResponse
-import uvicorn
+from urllib.request import urlopen
+from xml.etree.ElementTree import parse
+
 import datetime
-from dataclasses import dataclass
 import xml.etree.ElementTree as ET
 
 
-path = 't.xml'
+path = 'ostatki.zip'
 app = FastAPI()
 Token = 123
 
@@ -24,12 +25,39 @@ def update_yandex_table():
     json_data = {"password": "RAMTRX1500", "regulation": True, "email": "Rakhmanov-2019@list.ru"}
     response = requests.post('https://www.sima-land.ru/api/v5/signin', json=json_data)
     token = response.json().get('token')
-    zip = zipfile.ZipFile('ostatki.zip')
-    zip.extractall()
+
     session = requests.Session()
     retry = Retry(connect=2, backoff_factor=0.5)
     adapter = HTTPAdapter(max_retries=retry)
     session.mount('https://', adapter)
+
+    var_url = urlopen('https://give-ur-xml.herokuapp.com/')
+    xmldoc = parse(var_url)
+
+    for tag in xmldoc.iterfind('shop/offers/offer'):
+        try:
+            tag.remove(tag[1])
+        except Exception:
+            pass
+        response = session.get(
+            f"https://www.sima-land.ru/api/v5/item/{tag.attrib['id']}",
+            headers={
+                'accept': 'application/json',
+                'X-Api-Key': token,
+                'Authorization': token,
+            },
+
+            params={
+                'view': 'brief',
+                'by_sid': 'false',
+            }
+        )
+        print(response.json()['sid'])
+        if int(tag.find('count').text) < 10:
+            tag.find('count').text = '0'
+        else:
+            tag.find('count').text = str(response.json()['balance'])
+    xmldoc.write('t.xml', encoding='utf-8')
     zf = zipfile.ZipFile("ostatki.zip", "w", compresslevel=8, compression=zipfile.ZIP_DEFLATED)
     zf.write('t.xml', compresslevel=8)
 
@@ -38,3 +66,4 @@ def update_yandex_table():
 async def start(background_tasks: BackgroundTasks):
     background_tasks.add_task(update_yandex_table)
     return f"Started at {datetime.datetime.now()}"
+
