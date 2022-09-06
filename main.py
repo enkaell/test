03d2a67061efe0d7195e7f802e207b706be991dd
vlad <vlad2012chesnokov@mail.ru>
2022-09-06 09:51:40 +0500

    ss

--- main.py
+++ main.py
@@ -2,22 +2,31 @@ import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 import zipfile
-from flask import Flask, send_file
+from fastapi import FastAPI, BackgroundTasks
+from fastapi.responses import FileResponse
+import uvicorn
 import datetime
+from dataclasses import dataclass
 import xml.etree.ElementTree as ET
 
+
+@dataclass
+class Date:
+    date: str
+    info: str
+
+
 path = 'ostatki.zip'
-app = Flask(__name__)
+app = FastAPI()
 Token = 123
 
 
-@app.route('/')
-def main():
-    return send_file(path)
+@app.get("/", response_class=FileResponse)
+async def main():
+    return path
 
 
 def update_yandex_table():
-    print('started')
     json_data = {"password": "RAMTRX1500", "regulation": True, "email": "Rakhmanov-2019@list.ru"}
     response = requests.post('https://www.sima-land.ru/api/v5/signin', json=json_data)
     token = response.json().get('token')
@@ -30,33 +39,51 @@ def update_yandex_table():
     adapter = HTTPAdapter(max_retries=retry)
     session.mount('https://', adapter)
     for tag in root_node.findall('shop/offers/offer'):
-        response = session.get(
-            f"https://www.sima-land.ru/api/v5/item/{tag.attrib['id']}",
-            headers={
-                'accept': 'application/json',
-                'X-Api-Key': token,
-                'Authorization': token,
-            },
+        try:
+            tag.remove(tag[1])
+        except Exception:
+            pass
+        try:
+            response = session.get(
+                f"https://www.sima-land.ru/api/v5/item/{tag.attrib['id']}",
+                headers={
+                    'accept': 'application/json',
+                    'X-Api-Key': token,
+                    'Authorization': token,
+                },
 
-            params={
-                'view': 'brief',
-                'by_sid': 'false',
-            }
-        )
-        print(f"{response.json()['sid']} обновлен")
+                params={
+                    'view': 'brief',
+                    'by_sid': 'false',
+                }
+            )
+        except Exception as e:
+            tree.write('t.xml', encoding='utf-8')
+            zf = zipfile.ZipFile("ostatki.zip", "w", compresslevel=8, compression=zipfile.ZIP_DEFLATED)
+            zf.write('t.xml', compresslevel=8)
         if int(tag.find('count').text) < 10:
             tag.find('count').text = '0'
         else:
             tag.find('count').text = str(response.json()['balance'])
     tree.write('t.xml', encoding='utf-8')
+    zf = zipfile.ZipFile("ostatki.zip", "w", compresslevel=8, compression=zipfile.ZIP_DEFLATED)
+    zf.write('t.xml', compresslevel=8)
+    Date.date = str(datetime.datetime.now())
+    Date.info = "Ended"
 
 
-@app.route('/start')
-def start():
-    import threading
-    threading.Thread(target=update_yandex_table).start()
+@app.get("/start")
+async def start(background_tasks: BackgroundTasks):
+    background_tasks.add_task(update_yandex_table)
+    Date.date = str(datetime.datetime.now())
+    Date.info = "Started"
     return f"Started at {datetime.datetime.now()}"
 
 
+@app.get("/info")
+async def get_info():
+    return f"{Date.info} at {Date.date}"
+
+
 if __name__ == '__main__':
-    app.run(host ='0.0.0.0', port = 5001, debug = True)
+    uvicorn.run(app, host='127.0.0.1', port=8080)
