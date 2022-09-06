04339b486e603ceb622cc1070db2ed5095a931e4
vlad <vlad2012chesnokov@mail.ru>
2022-09-06 20:42:27 +0500

    ss

--- main.py
+++ main.py
@@ -26,36 +26,10 @@ def update_yandex_table():
     token = response.json().get('token')
     zip = zipfile.ZipFile('ostatki.zip')
     zip.extractall()
-    tree = ET.parse('t.xml')
-    root_node = tree.getroot()
     session = requests.Session()
     retry = Retry(connect=2, backoff_factor=0.5)
     adapter = HTTPAdapter(max_retries=retry)
     session.mount('https://', adapter)
-    for tag in root_node.findall('shop/offers/offer'):
-        try:
-            tag.remove(tag[1])
-        except Exception:
-            pass
-        response = session.get(
-                    f"https://www.sima-land.ru/api/v5/item/{tag.attrib['id']}",
-                    headers={
-                        'accept': 'application/json',
-                        'X-Api-Key': token,
-                        'Authorization': token,
-                    },
-
-                    params={
-                        'view': 'brief',
-                        'by_sid': 'false',
-                    }
-                )
-        print(response.json()['sid'])
-        if int(tag.find('count').text) < 10:
-            tag.find('count').text = '0'
-        else:
-            tag.find('count').text = str(response.json()['balance'])
-    tree.write('t.xml', encoding='utf-8')
     zf = zipfile.ZipFile("ostatki.zip", "w", compresslevel=8, compression=zipfile.ZIP_DEFLATED)
     zf.write('t.xml', compresslevel=8)
 
