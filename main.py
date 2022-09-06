3bb758675f9096cb17080440d256b19a60e9817c
vlad <vlad2012chesnokov@mail.ru>
2022-09-06 21:23:35 +0500

    test lxml parse

--- main.py
+++ main.py
@@ -35,33 +35,6 @@ def update_yandex_table():
     var_url = urlopen('https://give-ur-xml.herokuapp.com/')
     print('trying to parse xml...')
     xmldoc = etree.parse(var_url)
-    print('starting main loop...')
-    for tag in xmldoc.iterfind('shop/offers/offer'):
-        try:
-            tag.remove(tag[1])
-        except Exception:
-            pass
-        response = session.get(
-            f"https://www.sima-land.ru/api/v5/item/{tag.attrib['id']}",
-            headers={
-                'accept': 'application/json',
-                'X-Api-Key': token,
-                'Authorization': token,
-            },
-
-            params={
-                'view': 'brief',
-                'by_sid': 'false',
-            }
-        )
-        print(response.json()['sid'])
-        if int(tag.find('count').text) < 10:
-            tag.find('count').text = '0'
-        else:
-            tag.find('count').text = str(response.json()['balance'])
-    xmldoc.write('t.xml', encoding='utf-8')
-    zf = zipfile.ZipFile("ostatki.zip", "w", compresslevel=8, compression=zipfile.ZIP_DEFLATED)
-    zf.write('t.xml', compresslevel=8)
 
 
 @app.get("/start")
