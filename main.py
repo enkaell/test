a58946ca88395d36f58d8c961d88779d938255fe
vlad <vlad2012chesnokov@mail.ru>
2022-09-16 12:20:46 +0500

    back zip

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import uvicorn
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import FileResponse
 from dataclasses import dataclass
 
 import datetime
 import shutil
+import os
 
 app = FastAPI()
 Token = 123
@@ -12,26 +14,36 @@ Token = 123
 @dataclass
 class Path:
     path: str = 'ostatki.zip'
+    upload_url: str = os.path.dirname(os.path.abspath(__file__))
+
 
 path = Path()
 
 
 @app.get("/", response_class=FileResponse)
 async def main():
+    print('debug')
     return path.path
 
 
 @app.post("/upload")
 def upload(file: UploadFile):
     if ".zip" in file.filename:
-        with open(file.filename, 'wb') as f:
-            shutil.copyfileobj(file.file, f)
-        path.path = str(file.filename)
+        file_object = file.file
+        # create empty file to copy the file_object to
+        upload_folder = open(os.path.join(Path.upload_url, file.filename), 'wb+')
+        shutil.copyfileobj(file_object, upload_folder)
+        upload_folder.close()
+        path.path = file.filename
         return f"File {path.path} is uploaded"
     else:
         return "Wrong format"
 
 
 @app.get("/info")
-async def main():
+async def info():
     return f"Updated at {datetime.datetime.now()}"
+
+
+if __name__ == '__main__':
+    uvicorn.run(app=app)
